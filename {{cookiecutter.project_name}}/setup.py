0faaecd79ffc76eedd6ccc86d092aa3469ad6069
blivds06 <90219744+blivds06@users.noreply.github.com>
2022-09-05 09:07:56 -0500

    Update setup.py missing brackets

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="{{cookiecutter.package_name",
+    name="{{cookiecutter.package_name}}",
     version="0.1.0",
     packages=find_packages("support"),
     package_dir("": "support"),
